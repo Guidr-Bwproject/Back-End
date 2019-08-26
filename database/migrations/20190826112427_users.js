9fbfb98c559995e1e7ec673619f5f69b4be9086a
Yusuf Nafey <yusufnafey@yahoo.com>
2019-08-26 12:24:16 -0700

    trips table rough draft

--- database/migrations/20190826112427_users.js
+++ database/migrations/20190826112427_users.js
@@ -1,25 +1,40 @@
 exports.up = function(knex) {
-  return knex.schema.createTable("users", tbl => {
-    tbl.increments();
-    tbl.string("title");
-    tbl.string("tagline").notNullable();
-    tbl.integer("age");
-    tbl
-      .timestamp("created_at")
-      .notNullable()
-      .defaultTo(knex.raw("now()"));
-    tbl
-      .string("username", 255)
-      .notNullable()
-      .unique();
-    tbl.string("password", 255).notNullable();
-    tbl
-      .string("email", 255)
-      .notNullable()
-      .unique();
-  });
+  return knex.schema
+    .createTable("users", tbl => {
+      tbl.increments();
+      tbl.string("title", 255).notNullable();
+      tbl.string("tagline", 255).notNullable();
+      tbl.integer("age").notNullable();
+      tbl
+        .timestamp("created_at")
+        .defaultTo(knex.raw("now()"))
+        .notNullable();
+      tbl
+        .string("username", 255)
+        .unique()
+        .notNullable();
+      tbl.string("password", 255).notNullable();
+      tbl
+        .string("email", 255)
+        .unique()
+        .notNullable();
+    })
+    .createTable("trips", tbl => {
+      tbl.increments();
+      tbl.string("title", 255);
+      tbl.string("description", 4000);
+      tbl
+        .boolean("professional")
+        .defaultTo(false)
+        .notNullable();
+      tbl.string("time");
+      tbl.string("date");
+      tbl.string("location", 255);
+      tbl.integer("price");
+      tbl.string("image");
+    });
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists("users");
+  return knex.schema.dropTableIfExists("trips").dropTableIfExists("users");
 };

12f7e640262d469cc01ce75818c8530072d07ed1
Yusuf Nafey <yusufnafey@yahoo.com>
2019-08-26 12:37:36 -0700

    implements post endpoint for register

--- auth-router.js
+++ auth-router.js
@@ -1,6 +1,20 @@
 const router = require("express").Router();
 
-router.post("/register", (req, res) => {});
+const Users = require("./auth-model");
+
+router.post("/register", (req, res) => {
+  const user = req.body;
+
+  Users.add(user)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: "There was an error registering the new user. " });
+    });
+});
 
 router.post("/login", (req, res) => {});
 

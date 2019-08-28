9038563be8a7b57eb4aab5447802d3877c105a09
Yusuf Nafey <yusufnafey@yahoo.com>
2019-08-28 13:55:07 -0700

    changes migrations name, organizes files

--- api/server.js
+++ api/server.js
@@ -4,7 +4,6 @@ const helmet = require("helmet");
 
 const authRouter = require("./routers, models, middleware/authRouter");
 const tripRouter = require("./routers, models, middleware/tripRouter");
-// const authenticate = require("./routers, models, middleware/authMiddleware");
 
 const server = express();
 
@@ -15,7 +14,7 @@ server.use(express.json());
 server.use("/api/auth", authRouter);
 server.use("/api/trips", tripRouter);
 
-// TEST
+// ================= SERVER TEST =================
 server.get("/", (req, res) => {
   res.status(200).json({ server: "running" });
 });
@@ -46,11 +45,6 @@ module.exports = server;
 // }
 
 // QUESTIONS:
-// are my models okay? do I need findTripById AND findUserById in model.js?
-// where am I supposed to put the authentication middleware? I put them in the last 3 trip routers but it feels wrong.
-// data in the tables
-// do I need any endpoint for portfolio page (allows user to add title, tagline, age, and length of time spent as a guide)
-// time for users table
 
 // TO DO:
 // add comments
